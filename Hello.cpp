b2f9eb66ee7ac5606bc951198086dafacd431857
alanheng33 <alanheng@cpp.edu>
2025-01-27 21:04:38 -0800

    created hello world program
    created and added hello.cpp

---
+++ Hello.cpp
@@ -0,0 +1,13 @@
+/*
+Program for outputing "Hello World" using two different command lines
+*/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    cout << "Hello World!" << endl;
+    printf("Hello Worlda again!\n");
+    return 0;
+}
